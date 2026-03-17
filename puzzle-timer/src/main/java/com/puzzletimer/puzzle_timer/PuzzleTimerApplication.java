2755943d1e14c84e93e7f8a0505842484162688d
Lucifer Veneris <luxveneris6661@gmail.com>
2026-03-17 11:39:18 +0900

    feat: add puzzle-timer Spring Boot project

---
+++ puzzle-timer/src/main/java/com/puzzletimer/puzzle_timer/PuzzleTimerApplication.java
@@ -0,0 +1,13 @@
+package com.puzzletimer.puzzle_timer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzleTimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzleTimerApplication.class, args);
+	}
+
+}
---
+++ puzzle-timer/src/test/java/com/puzzletimer/puzzle_timer/PuzzleTimerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puzzletimer.puzzle_timer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzleTimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
